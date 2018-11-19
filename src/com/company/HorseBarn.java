969aef1e3664e4bdbdb23389d10b8b119b39ebf8
BT_1N3_13 <a>
2018-11-19 08:19:27 -0500

    Updated HorseBarn

--- src/com/company/HorseBarn.java
+++ src/com/company/HorseBarn.java
@@ -1,13 +1,21 @@
 package com.company;
 
 public class HorseBarn{
+
+    private Horse[] spaces;
+
+
     public int findHorseSpace(String name){
         int index = -1;
         for (int i = 0 ; i < spaces.length(); i++){
             if (spaces[i].equals(name)){
                 index = i;
-                return index;
             }
         }
+        return index;
+    }
+
+    public void consolitdate() {
+        
     }
 }
