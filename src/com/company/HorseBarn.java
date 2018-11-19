65c6e4972b0d14109b3fa2e5846fde7a394f6671
BT_1N3_13 <a>
2018-11-19 08:37:48 -0500

    Added consolidate() to HorseBarn

--- src/com/company/HorseBarn.java
+++ src/com/company/HorseBarn.java
@@ -1,21 +1,33 @@
 package com.company;
 
-public class HorseBarn{
+public class HorseBarn {
 
     private Horse[] spaces;
 
 
-    public int findHorseSpace(String name){
+    public int findHorseSpace(String name) {
         int index = -1;
-        for (int i = 0 ; i < spaces.length(); i++){
-            if (spaces[i].equals(name)){
+        for (int i = 0; i < spaces.length; i++) {
+            if (spaces[i].equals(name)) {
                 index = i;
             }
         }
         return index;
     }
 
-    public void consolitdate() {
-        
+    public void consolidate() {
+        for (int i = 0; i < this.spaces.length - 1; i++) {
+            if (this.spaces[i] == null) {
+                int a = i + 1;
+                while (a < this.spaces.length) {
+                    if (this.spaces[a] != null) {
+                        this.spaces[i] = this.spaces[a];
+                    } else {
+                        a++;
+                    }
+                }
+            }
+
+        }
     }
 }
